90a27afa29f3c4570d303f30fc1834229905d436
Flidi <60918348+Flidi@users.noreply.github.com>
2020-08-28 17:18:41 +0300

    pull
    pull

---
+++ New Unity Project (2)/Assets/script1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script1 : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("Pressed primary button 1.");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
